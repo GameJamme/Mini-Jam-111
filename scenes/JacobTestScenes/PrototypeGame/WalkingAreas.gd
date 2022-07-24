5b7cebd3dadc81ff0704ee90269420fe2da9622f
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2022-07-24 13:09:04 -0500

    Added music areas that interact with the musicPlayer singleton when the player enters a new area

--- scenes/JacobTestScenes/PrototypeGame/WalkingAreas.gd
+++ scenes/JacobTestScenes/PrototypeGame/WalkingAreas.gd
@@ -5,27 +5,30 @@ signal type_changed(type)
 
 var current_type : int = 1
 
-
-func _ready():
-	_on_StoneArea_body_entered(null)
-
 func _on_StoneArea_body_entered(body):
+	if _body_not_player(body):
+		return
 	current_type = FootstepManager.GroundType.stone
 	emit_signal("type_changed", current_type)
 
 
 func _on_StoneArea_body_exited(body):
+	if _body_not_player(body):
+		return
 	if current_type == FootstepManager.GroundType.stone:
 		_default_to_grass()
 
 
 func _on_WoodArea_body_entered(body):
-	if current_type == FootstepManager.GroundType.stone: return
+	if _body_not_player(body):
+		return
 	current_type = FootstepManager.GroundType.wood
 	emit_signal("type_changed", current_type)
 
 
 func _on_WoodArea_body_exited(body):
+	if _body_not_player(body):
+		return
 	if current_type == FootstepManager.GroundType.wood:
 		_default_to_grass()
 
@@ -33,3 +36,7 @@ func _on_WoodArea_body_exited(body):
 func _default_to_grass():
 	current_type = FootstepManager.GroundType.grass
 	emit_signal("type_changed", current_type)
+
+
+func _body_not_player(body) -> bool:
+	return not body.is_in_group("player")
